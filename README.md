8abdcf6d75a420c8f265f3aa64e2e03dffeb21a1
evanhackett <evanwhackett@gmail.com>
2017-01-29 19:59:04 -0800

    Init commit. Mostly copied things over from "demons of the cyclic space".

---
+++ README.md
@@ -0,0 +1,7 @@
+This is the beginning of a cellular automata creator. Users will be able to enter a callback function which will define the rules of a CA.
+Looping through the grid, coloring cells, and getting neighboring cells will be abstracted away.
+
+To start the dev environment run:
+    npm run-script watch-js
+
+That will watch js files and build with browserify.
---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+
+const _ = require('lodash')
+const Grid = require('./CA_grid.js')
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+const width = canvas.width
+const height = canvas.height
+const CANVAS_RESOLUTION = width
+const CA_RESOLUTION = 10
+
+function draw(grid) {
+    //clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+
+    grid.each(function(cell, i, j) {
+        colorCell(i, j, getCellColor(cell))
+    })
+}
+
+function colorCell(x, y, color) {
+    // calculate dimensions of a cell based on the grid resolution and the canvas resolution
+    const cellSize = CANVAS_RESOLUTION / CA_RESOLUTION
+    ctx.fillStyle = `rgb(${color.r},${color.g},${color.b})`
+    ctx.fillRect(x*cellSize, y*cellSize, cellSize, cellSize)
+}
+
+function getCellColor(value) {
+    const map = {
+        0 : {r: 0, g: 0, b: 0},
+        1 : {r: 0, g: 0, b: 255},
+        2 : {r: 0, g: 255, b: 0},
+        3 : {r: 0, g: 255, b: 255},
+        4 : {r: 255, g: 0, b: 0},
+        5 : {r: 255, g: 0, b: 255},
+        6 : {r: 255, g: 255, b: 0},
+        7 : {r: 255, g: 255, b: 255},
+        8 : {r: 127, g: 127, b: 255},
+        9 : {r: 255, g: 255, b: 127},
+        10 : {r: 0, g: 127, b: 255},
+        11 : {r: 255, g: 127, b: 0},
+    }
+    return map[value]
+}
+
+const grid = new Grid(CA_RESOLUTION, CANVAS_RESOLUTION)
+grid.initialize()
+
+
+setInterval(function() {
+    draw(grid)
+    grid.nextState(nextState)
+}, 1000)
+
+// here is the user-defined nextState function. For now we will do a simple cyclic CA.
+// TODO: allow user to enter in their own nextState function.
+function nextState(cell) {
+    const predicate = function(neighbor) {
+        if (cell.value === 11) {
+            return neighbor.value === 0
+        }
+        return neighbor.value === cell.value + 1
+    }
+
+    if (_.filter(cell.neighbors, predicate).length > 0) {
+        return cell.value === 11 ? 0 : cell.value + 1
+    }
+    return cell.value
+}
